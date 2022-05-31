ac47e1b5f3bc6c4c21e2cec4e8d438f75a8940b0
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2022-05-31 18:52:22 +0530

    Fixed the bot admin

--- apps/develop/admin.py
+++ apps/develop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import DevProfile, DocumentationCategory, Documentation, Bot
+from .models import DevProfile, DocumentationCategory, Documentation, Bot, WebHook
 class DocumentationAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'description', 'category', 'date')
@@ -17,11 +17,18 @@ class DocumentationCategoryAdmin(admin.ModelAdmin):
     list_per_page = 10
     verbose_name_plural = 'Documentation Categories'
 
+class WebHookInline(admin.StackedInline):
+    '''Stacked Inline View for WebHook'''
+
+    model = WebHook
+    extra = 3
+
 class BotAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner')
     search_fields = ('name', 'user', 'owner')
     list_per_page = 10
-    fields = ('name', 'description', 'user', 'owner', 'endpoint', 'private_key')
+    inlines = [WebHookInline]
+    fields = ('name', 'description', 'user', 'owner', 'private_key')
     readonly_fields = ('user', 'owner', 'private_key')
 
 admin.site.register(Bot, BotAdmin)
