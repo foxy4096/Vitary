67b07a2a6aeade092c03d88242bfc26dcdba9259
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2022-06-22 19:46:29 +0530

    Removed django CORS Headers

--- Vitary/settings.py
+++ Vitary/settings.py
@@ -69,7 +69,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Vitary.urls'
