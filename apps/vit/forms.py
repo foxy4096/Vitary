8c3a2c799d979ff276df6920763fec5486ae9c99
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2022-11-29 21:16:02 +0530

    Added profile in vit add form

--- apps/vit/forms.py
+++ apps/vit/forms.py
@@ -15,7 +15,7 @@ class VitForm(forms.ModelForm):
                 attrs={
                     "class": "textarea is-medium",
                     "placeholder": "What's on your mind?",
-                    "style": """height: 75px;""",
+                    "style": """height: 150px;""",
                     "id": "body",
                 }
             ),
